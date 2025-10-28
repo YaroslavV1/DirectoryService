18e7c62e9d1e67c9d2fa8ed919191750a25e62d7
YaroslavV1 <gaslotv@gmail.com>
2025-10-28 22:37:31 +0200

    Added Serilog Added Seq from docker Used logging in the project

--- DirectoryService/src/DirectoryService.Presentation/Program.cs
+++ DirectoryService/src/DirectoryService.Presentation/Program.cs
@@ -2,14 +2,29 @@ using System.Text.Json.Serialization;
 using DirectoryService.Infrastructure;
 using DirectoryService.Presentation;
 using Microsoft.AspNetCore.Http.Json;
+using Serilog;
+using Serilog.Events;
 
 var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Debug()
+    .WriteTo.Console()
+    .WriteTo.Seq(builder.Configuration.GetConnectionString("Seq")
+                 ?? throw new ArgumentNullException("Seq"))
+    .MinimumLevel.Override("Microsoft.AspNetCore.Hosting", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.AspNetCore.Mvc", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.AspNetCore.Routing", LogEventLevel.Warning)
+    .CreateLogger();
+
 
 builder.Services.AddScoped<DirectoryServiceDbContext>(sp =>
     new DirectoryServiceDbContext(
         builder.Configuration.GetConnectionString("DirectoryServiceDb")!));
 
 builder.Services.AddProgramDependencies();
+
+builder.Services.AddSerilog();
 
 builder.Services.Configure<JsonOptions>(options =>
 {
@@ -23,6 +38,8 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
     app.UseSwaggerUI(options => options.SwaggerEndpoint("/openapi/v1.json", "DirectoryService"));
 }
+
+app.UseSerilogRequestLogging();
 
 app.MapControllers();
 
