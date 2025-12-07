9d3c5065471c1d0d688289c826a3c397bdb09ab9
YaroslavV1 <gaslotv@gmail.com>
2025-12-07 18:31:04 +0200

    Add service to docker with dockerfile and docker-compose file

---
+++ DirectoryService/src/DirectoryService.Presentation/Extensions/AppExtensions.cs
@@ -0,0 +1,18 @@
+﻿using DirectoryService.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace DirectoryService.Presentation.Extensions;
+
+public static class AppExtensions
+{
+    public static async Task<WebApplication> ApplyMigrations(this WebApplication app)
+    {
+        await using var scope = app.Services.CreateAsyncScope();
+
+        var dbContext = scope.ServiceProvider.GetRequiredService<DirectoryServiceDbContext>();
+
+        await dbContext.Database.MigrateAsync();
+
+        return app;
+    }
+}
